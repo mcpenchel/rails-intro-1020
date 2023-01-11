96d8587bdff17c466e6c6e21028b848392efe390
Matheus Penchel <matheus.penchel@gmail.com>
2023-01-10 21:22:45 -0300

    Recap da aula

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+  def best_band
+    @godzilla_enemies = ['King Ghidorah', 'Mothra', 'Gorosaurus', 'Baragon']
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # *Eu* decidi que o nome do meu controller vai ser PagesController
+
+  root to: 'pages#best_band'
+
+  get 'about',      to: 'pages#about',   as: :about
+  get 'contact_us', to: 'pages#contact', as: :contact
+
+  # post 'a_url_que_eu_queria', to: 'o_meu_controller_no_plural#o_nome_da_acao'
+  # put 'a_url_que_eu_queria', to: 'o_meu_controller_no_plural#o_nome_da_acao'
+  # delete 'a_url_que_eu_queria', to: 'o_meu_controller_no_plural#o_nome_da_acao'
 end
